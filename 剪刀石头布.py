ce6f6e9e87a4752b8f4afef613686a2fbf6e19e5
qzxyj <84173579+qzxyj@users.noreply.github.com>
2021-05-14 21:28:05 +0800

    Add files via upload
    剪刀石头布，Python

---
+++ 剪刀石头布.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import random
+# 获胜次数
+win_times = 0
+number = 0
+choice = ['0','1','2']
+punches=['剪刀','石头','布']
+while win_times <= 1:
+  print('第'+str(win_times+1)+'局')
+  while True:
+    player = input('请输入：剪刀(0)  石头(1)  布(2):')
+    if player in choice:
+      break
+    else:
+      print('输入有误，请重输！')
+  number+=1
+  computer = random.choice(choice)
+  print("你出了：",punches[int(player)])
+  print("电脑出了：",punches[int(computer)])
+  if player == choice[choice.index(computer)-1]:
+    print('输了，不要走，洗洗手接着来，决战到天亮')
+  elif player == computer:
+    print('平局，要不再来一局')
+  else:
+    win_times += 1
+    print('获胜，哈哈，你太厉害了')
+print("共出了"+str(number)+'手')
